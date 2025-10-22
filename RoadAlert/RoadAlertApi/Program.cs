a5925f74161c862153648da86e9ada5c0ea134a4
maljen01 <jens.malm@trafikverket.se>
2025-10-22 19:21:11 +0200

    dbcontext och swagger i startup

--- RoadAlert/RoadAlertApi/Program.cs
+++ RoadAlert/RoadAlertApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using RoadAlertApi.Models;
+
 namespace RoadAlertApi
 {
     public class Program
@@ -7,12 +10,15 @@ namespace RoadAlertApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddDbContext<VehicleAlertsContext>(o => o.UseSqlServer("Server=.;Database=VehicleAlerts;Trusted_Connection=True;Encrypt=False;"));
             builder.Services.AddControllers();
-
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
