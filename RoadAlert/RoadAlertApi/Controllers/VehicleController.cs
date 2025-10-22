b9e04545cfaac9e191c2795009a5b56cc4147051
Jonas Gauffin <jonas.gauffin@coderr.io>
2025-10-22 18:49:35 +0200

    Added controller for vehicles

---
+++ RoadAlert/RoadAlertApi/Controllers/VehicleController.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using RoadAlertApi.Models;
+using RoadAlertApi.Models.Vehicles;
+
+namespace RoadAlertApi.Controllers
+{
+    public class VehicleController:Controller
+    {
+        private VehicleAlertsContext _dbContext;
+
+        public VehicleController(VehicleAlertsContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IReadOnlyList<StoppedVehicle> Stopped()
+        {
+            var expr = from tpms in _dbContext.Tpms
+                from gps in _dbContext.GnssPositions
+                where tpms.AlertId == gps.AlertId
+                      && tpms.PressureActual < 140
+                      && gps.Speed < 10
+                select new StoppedVehicle()
+                {
+                    Lat = gps.Latitude.Value, Lon = gps.Longitude.Value, Heading = gps.Heading.Value,
+                    RegisteredAt = gps.PositionDateTime.Value
+                };
+
+
+            return expr.ToList();
+            //_dbContext.Tpms.Where(x=>x.PressureActual < 140)
+            //    .Join(x=>x.)
+        }
+    }
+}
---
+++ RoadAlert/RoadAlertApi/Models/Vehicles/StoppedVehicle.cs
@@ -0,0 +1,10 @@
+﻿namespace RoadAlertApi.Models.Vehicles;
+
+public class StoppedVehicle
+{
+    public double Lat { get; init; }
+    public double Lon { get; set; }
+    public DateTime RegisteredAt { get; set; }
+
+    public int Heading { get; set; }
+}
